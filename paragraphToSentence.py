5ad4a88cd97d4836af46a23b9ee44c12968911f3
adwin5 <adwin5@kimo.com>
2016-09-05 00:05:00 -0700

    add lines for close open file

--- paragraphToSentence.py
+++ paragraphToSentence.py
@@ -4,10 +4,12 @@ sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
 input_file_name = "input.txt"
 output_file_name = "output.txt"
 
-with open(input_file_name, 'r') as myfile:
-    data=myfile.read().decode("utf-8").replace('\n', '')
+with open(input_file_name, 'r') as input_file:
+    data=input_file.read().decode("utf-8").replace('\n', '')
 # print '\n---\n'.join(sent_detector.tokenize(data.strip()))
 
 output_file = open(output_file_name, 'w')
 for item in sent_detector.tokenize(data.strip()):
   output_file.write("%s\n" % item.encode('utf-8'))
+input_file.close()
+output_file.close()
